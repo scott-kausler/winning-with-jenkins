52f87a16770dd9f95d69d19b3196c192e43f4f47
Scott Kausler <skausler@lovevery.com>
2021-06-09 21:02:59 -0600

    I don't know what wizardry I would use to do this other then copy/paste

--- jenkins/jobs/utilities/JobDSLUtilities.groovy
+++ jenkins/jobs/utilities/JobDSLUtilities.groovy
@@ -7,65 +7,6 @@ class JobDSLUtilities {
         }
     }
 
-    static createMultibranchPipelineJob(script, String githubOrg, String githubRepo) {
-
-        def jobName="build"
-        String jenkinsfile = "jenkins/pipelines/build-merge.jenkins"
-        script.multibranchPipelineJob("${githubRepo}/${jobName}") {
-            displayName("${jobName}")
-            branchSources {
-                branchSource {
-                    source {
-                        github {
-                        //https://issues.jenkins.io/browse/JENKINS-43693
-                        id(UUID.nameUUIDFromBytes("${githubRepo}/${jobName}".getBytes()).toString())
-                        repoOwner(githubOrg)
-                        repository("${githubRepo}")
-                        credentialsId("github-credentials")
-                        repositoryUrl("https://github.com/${githubOrg}/${githubRepo}.git")
-                        configuredByUrl(false)
-                        apiUri('https://api.github.com')
-                        traits {
-                            gitHubBranchDiscovery {
-                                strategyId(2)
-                            }
-                            headWildcardFilter {
-                                includes(branches)
-                                excludes("")
-                            }
-                        }
-                        }
-                    }
-                    buildStrategies {
-                        buildAllBranches {
-                            strategies {
-                                skipInitialBuildOnFirstBranchIndexing()
-                            }
-                        }
-                    }
-                }
-            }
-
-            orphanedItemStrategy {
-                discardOldItems {
-                    daysToKeep(10)
-                    numToKeep(10)
-                }
-            }
-
-            // check every minute for scm changes as well as new / deleted branches
-            triggers {
-                periodic(1)
-            }
-
-            factory {
-                workflowBranchProjectFactory {
-                scriptPath(jenkinsfile)
-                }
-            }
-        }
-    }
-
     static createPipelineJob(script, String githubOrg, String githubRepo, String jobName="merge", String jenkinsfile="jenkins/pipelines/build-merge.jenkins") {
         script.pipelineJob("${githubRepo}/" + jobName) {
             displayName(jobName)
