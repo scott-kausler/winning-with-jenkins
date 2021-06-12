aec8c1025869c9e95d3fca79ee528dd0d81d53c4
scott-kausler <56176260+scott-kausler@users.noreply.github.com>
2021-06-11 22:59:31 -0600

    Use plugins to get github data (#2)
    * Use plugins to get github data
    
    * Missing a comment of course...
    
    * Whange stuff
    
    * Found it
    
    * Text
    
    * Still working through it
    
    * Still working through it
    
    * Still working through it
    
    * Still working through it
    
    * Still working through it
    
    * Remove this
    
    * Still working through it
    
    * Ugh
    
    Co-authored-by: Scott Kausler <skausler@lovevery.com>

--- src/lib/Github.groovy
+++ src/lib/Github.groovy
@@ -42,25 +42,22 @@ class Github implements Serializable {
         }
 
         script.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'github-credentials', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_PASS']]) {
-        def githubOrgAndRepo=getOrgAndRepoName(script)
-        def branch = getBranchName(script)
+            def githubOrgAndRepo=getOrgAndRepoName(script)
+            def branch = getBranchName(script)
 
-        prNumber=script.sh(script: """
-            URL="https://api.github.com/repos/$githubOrgAndRepo/pulls?head=lovevery-digital:${branch}&state=open"
-            #remove whitespace
-            URL=\$(echo \$URL)
-            curl -s -f -H "Authorization: token $script.GITHUB_PASS" "\$URL" > pr.json
-            PR_COUNT=\$(jq -r length pr.json)
-            if [ "\$PR_COUNT" != "1" ]; then
-                echo "-1"
-            else
-                PR_NUMBER=\$(jq -rj '.[0].number' pr.json)
-                echo "\$PR_NUMBER"
-            fi
-        """, returnStdout: true).replaceAll("\\s", "")
+            def response = script.httpRequest(
+                url: "https://api.github.com/repos/$githubOrgAndRepo/pulls?head=${branch}&state=open",
+                acceptType: "APPLICATION_JSON",
+                customHeaders: [[name: "Authorization", value: "token $script.GITHUB_PASS"]],
+                validResponseCodes: "200,201"
+            )
+
+            def props = script.readJSON text: response.content
+            if(response.content.size() < 5) {
+                return "-1"
+            }
+            return props[0].number
         }
-
-        return prNumber;
     }
 
     public static void addStatusCheck(script, contextSuffix, description, url) {
@@ -81,31 +78,30 @@ class Github implements Serializable {
         def encodedBranchName=java.net.URLEncoder.encode("$branch", "UTF-8")
         def repoName=getRepoName(script)
 
-        String url = "${script.JENKINS_URL}job/$repoName/job/$jobName/parambuild?GIT_BRANCH=$encodedBranchName"
+        String url = "${script.JENKINS_URL}job/$repoName/job/$jobName/parambuild?BRANCH_NAME=$encodedBranchName"
         String statusText = "Click Details button to go to $jobName job"
 
         addStatusCheck(script, "$jobName-job", statusText, url)
     }
 
     public static merge(script) {
-        def prNumber = getPRNumber(script)
-        if(prNumber == "-1"){
-            script.echo "No PR number. Cannot merge"
-            return
-        }
-
-        if(!script.env.JOB_NAME.contains("/merge")){
+        if(!script.env.JOB_NAME.contains("/merge")) {
             script.echo "Can only merge merge jobs"
             return
         }
 
+        def prNumber = getPRNumber(script)
         def githubOrgAndRepo=getOrgAndRepoName(script)
+
         script.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'github-credentials', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_PASS']]) {
-            script.sh(script: """
-                URL="https://api.github.com/repos/${githubOrgAndRepo}/pulls/${prNumber}/merge"
-                URL=\$(echo \$URL)
-                curl -f -H "Authorization: token ${script.GITHUB_PASS}"  -X PUT -d '{"merge_method": "squash"}'  "\$URL"
-            """)
+            script.httpRequest(
+                url: "https://api.github.com/repos/$githubOrgAndRepo/pulls/${prNumber}/merge",
+                customHeaders: [[name: "Authorization", value: "token $script.GITHUB_PASS"]],
+                contentType: "APPLICATION_JSON",
+                requestBody: '{"merge_method": "squash"}',
+                httpMode: "PUT",
+                validResponseCodes: "200,201"
+            )
         }
     }
 }
